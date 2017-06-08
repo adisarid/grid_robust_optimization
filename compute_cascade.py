685b3708f48bd119ff659398735cf427f97bbc27
adisarid <adisarid@gmail.com>
2017-06-08 14:41:28 +0300

    Added a file (blank) for cascade algorithm

---
+++ compute_cascade.py
@@ -0,0 +1,11 @@
+#-------------------------------------------------------------------------------
+# Name:        compute_cascade
+# Purpose:     Computes grid cascade based on the Sultan and Zussman algorithm.
+#
+# Author:      Adi Sarid
+#
+# Created:     08/06/2017
+# Copyright:   (c) Adi Sarid 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
