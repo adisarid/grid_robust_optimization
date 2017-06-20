fe22338996c75106924c62f842912bfea28ec16d
adisarid <adisarid@gmail.com>
2017-06-20 13:57:19 +0300

    Small function to write results to csv

--- export_results.py
+++ export_results.py
@@ -9,3 +9,11 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 
+def write_names_values(current_solution, variable_names, csvfilename):
+    var_row = [[variable_names[i], current_solution[i]] for i in range(len(variable_names))]
+    with open(csvfilename, 'wb') as csvfile:
+        solutionwriter = csv.writer(csvfile, delimiter = ',')
+        solutionwriter.writerow(['name', 'value'])
+        solutionwriter.writerows(var_row)
+
+
