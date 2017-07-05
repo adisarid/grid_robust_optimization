b5e4b51d13027ec46d001da3e63d39466481bd59
adisarid <adisarid@gmail.com>
2017-07-05 17:59:09 +0300

    Should add constraints for init failures flow disabling

--- build_problem.py
+++ build_problem.py
@@ -150,6 +150,11 @@ def build_cplex_problem():
 
             for cur_edge in assoc_edges['out']:
                 # using only outgoing edges incoming will be covered as "outgoing" at a different node
+                # First set failed edges according to input data
+                # DEBUG HERE 05/07/2017.
+                init_failures = []
+                init_failures_coef = [1,-1]
+
                 # Phase angle constraints -M*F_ij <= theta_i-theta_j-x_ij*f_ij <= M*F_ij
                 # Less than equal side
                 phase_lhs = [dvar_pos[('theta', cur_node, scenario)], dvar_pos[('theta', cur_edge[1], scenario)], dvar_pos[('f', cur_edge, scenario)], dvar_pos[('F', cur_edge, scenario)]]
