b7947337a4942b696f6a4663bd8535c54c851486
adisarid <adisarid@gmail.com>
2017-09-26 13:05:30 +0300

    Added debug global file

---
+++ debug_output_specs.py
@@ -0,0 +1,14 @@
+#-------------------------------------------------------------------------------
+# Name:        Debug output specs
+# Purpose:     Define debug global variables
+#
+# Author:      Adi Sarid
+#
+# Created:     18/09/2017
+# Copyright:   (c) Adi Sarid 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+print_debug = True
+write_res_file = True
+write_lp_file = False #lpfilename = "c:/temp/grid_cascade_output/lp_form/single_type1_step" + str(i) + ".lp" # For debugging purpuses I added writing the lp files. Disabled by default
