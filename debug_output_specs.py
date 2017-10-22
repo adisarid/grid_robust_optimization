8d35c2710d92bac309b4a1d7eda56cc33089a292
adisarid <adisarid@gmail.com>
2017-10-22 19:59:49 +0300

    Added debug capabilities

--- debug_output_specs.py
+++ debug_output_specs.py
@@ -11,7 +11,8 @@
 
 print_debug = True
 print_degub_verbose = True
-write_mid_run_res_files = False
-write_res_file = True
+write_mid_run_res_files = True
+write_res_file = False
 write_lp_file = False #"c:/temp/grid_cascade_output/lp_form/single_type1_step" + str(i) + ".lp" # For debugging purpuses I added writing the lp files. Disabled by default
+print_cfe_results = True # should I print each cfe simulation results - which edges failed and which survived?
 limit_lazy_add = -1 # should I limit the number of lazy constraints added at each iteration. Use -1 for unlimited.
