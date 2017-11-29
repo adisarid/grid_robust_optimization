2f503a25fd26d622c9acc0b15d6ad932f1943c75
adisarid <adisarid@gmail.com>
2017-11-29 16:00:27 +0200

    Updated capacity upgrade coefficient scale (easier to solve)

--- global_definitions.py
+++ global_definitions.py
@@ -11,5 +11,5 @@
 
 instance_location = 'c:\\Users\\Adi Sarid\\Documents\\GitHub\\grid_robust_opt\\case30\\' #adi_simple2_discrete\\'
 line_cost_coef_scale = 1 #15 # coefficient to add to transmission line capacity variable to scale cost for binary instead of continuouos
-line_capacity_coef_scale = 5 # the value added by establishing an edge. initilized here temporarily. will be added later on to original data file (grid_edges.csv)
+line_capacity_coef_scale = 10 # the value added by establishing an edge. initilized here temporarily. will be added later on to original data file (grid_edges.csv)
 best_incumbent = 0
