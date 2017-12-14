118f5be8a98cc07ab9955b676411237b24229157
adisarid-ext <adisarid.python@gmail.com>
2017-12-14 10:39:51 +0200

    Added batch file for case30 runs

---
+++ batchrun_case30.bat
@@ -0,0 +1,7 @@
+rem Runs the robustness optimization problem
+rem Parameters:
+rem CASE_LOCATION TOTAL_RUN_TIME PROPORTION_CASES_SHORTRUNS USE_PRIORITIES
+python main_program.py 'case30' 0.5 0.0 True
+python main_program.py 'case30' 0.5 0.0 False
+python main_program.py 'case30' 0.5 0.5 False
+python main_program.py 'case30' 0.5 0.5 True
