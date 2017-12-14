5df1fb77a0e71c4efb977c2f087f1c2dbe5e1f98
adisarid-ext <adisarid.python@gmail.com>
2017-12-14 10:42:05 +0200

    added a run to case30 batch

--- batchrun_case30.bat
+++ batchrun_case30.bat
@@ -3,5 +3,5 @@ rem Parameters:
 rem CASE_LOCATION TOTAL_RUN_TIME PROPORTION_CASES_SHORTRUNS USE_PRIORITIES
 python main_program.py 'case30' 0.5 0.0 True
 python main_program.py 'case30' 0.5 0.0 False
-python main_program.py 'case30' 0.5 0.5 False
 python main_program.py 'case30' 0.5 0.5 True
+python main_program.py 'case30' 0.5 0.75 True
