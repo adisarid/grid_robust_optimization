97b754509da1f82ea764a23d63d1cbf3547f67ab
adisarid <adisarid@gmail.com>
2017-12-26 15:17:19 +0200

    Updated batch run commands and deleted previous solution_statistics res file

--- batchrun_case30.bat
+++ batchrun_case30.bat
@@ -1,7 +1,8 @@
 rem Runs the robustness optimization problem
 rem Parameters:
-rem CASE_LOCATION TOTAL_RUN_TIME PROPORTION_CASES_SHORTRUNS USE_PRIORITIES
-python main_program.py case30 0.5 0.0 True
-python main_program.py case30 0.5 0.0 False
-python main_program.py case30 0.5 0.5 True
-python main_program.py case30 0.5 0.75 True
+rem CASE_LOCATION TOTAL_RUN_TIME PROPORTION_CASES_SHORTRUNS USE_PRIORITIES LINE_CAPACITY_COEF_SCALE
+python main_program.py case30 3 0.0 True 15
+python main_program.py case30 3 0.0 False 15
+python main_program.py case30 3 0.0 True 5
+python main_program.py case30 3 0.0 True 10
+python main_program.py case30 3 0.0 True 20
