0b2a2c8965e3b3cfa96efa63a1b54f688aa4098f
adisarid <adisarid@gmail.com>
2018-01-21 21:30:23 +0200

    Added batch file for running the one cascade program

---
+++ temprunbatch.bat
@@ -0,0 +1,6 @@
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "case30" --disable_cplex_messages --budget 0
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "case30" --disable_cplex_messages --budget 2
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "case30" --disable_cplex_messages --budget 4
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "case30" --disable_cplex_messages --budget 6
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "case30" --disable_cplex_messages --budget 8
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "case30" --disable_cplex_messages --budget 10
