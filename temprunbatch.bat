254216cbb72ae97c64d399abca2eadbacaf8ca47
adisarid <adisarid@gmail.com>
2018-01-29 11:31:20 +0200

    Modified batch to run one cascade algorithm

--- temprunbatch.bat
+++ temprunbatch.bat
@@ -1,6 +1,13 @@
-python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "case30" --disable_cplex_messages --budget 0
-python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "case30" --disable_cplex_messages --budget 2
-python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "case30" --disable_cplex_messages --budget 4
-python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "case30" --disable_cplex_messages --budget 6
-python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "case30" --disable_cplex_messages --budget 8
-python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "case30" --disable_cplex_messages --budget 10
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "case30" --export_results_file --time_limit 1.0 --budget 0
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "case30" --export_results_file --time_limit 1.0 --budget 25
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "case30" --export_results_file --time_limit 1.0 --budget 50
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "case30" --export_results_file --time_limit 1.0 --budget 100
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "case30" --export_results_file --time_limit 1.0 --budget 150
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "case30" --export_results_file --time_limit 1.0 --budget 200
+
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "case30" --export_results_file --time_limit 1.0 --budget 0 --penalize_failures 0.1
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "case30" --export_results_file --time_limit 1.0 --budget 25 --penalize_failures 0.1
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "case30" --export_results_file --time_limit 1.0 --budget 50 --penalize_failures 0.1
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "case30" --export_results_file --time_limit 1.0 --budget 100 --penalize_failures 0.1
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "case30" --export_results_file --time_limit 1.0 --budget 150 --penalize_failures 0.1
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "case30" --export_results_file --time_limit 1.0 --budget 200 --penalize_failures 0.1
