270e676e221f885f457790ff1f8ff885ad0bcb6e
adisarid <adisarid@gmail.com>
2018-05-04 17:26:16 +0300

    Updated batch run file for new instances

--- temprunbatch.bat
+++ temprunbatch.bat
@@ -1,13 +1,41 @@
-python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "case30" --export_results_file --time_limit 1.0 --budget 0
-python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "case30" --export_results_file --time_limit 1.0 --budget 25
-python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "case30" --export_results_file --time_limit 1.0 --budget 50
-python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "case30" --export_results_file --time_limit 1.0 --budget 100
-python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "case30" --export_results_file --time_limit 1.0 --budget 150
-python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "case30" --export_results_file --time_limit 1.0 --budget 200
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "instance24" --export_results_file --time_limit 1.0 --budget 0
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "instance24" --export_results_file --time_limit 1.0 --budget 25
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "instance24" --export_results_file --time_limit 1.0 --budget 50
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "instance24" --export_results_file --time_limit 1.0 --budget 100
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "instance24" --export_results_file --time_limit 1.0 --budget 150
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "instance24" --export_results_file --time_limit 1.0 --budget 200
 
-python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "case30" --export_results_file --time_limit 1.0 --budget 0 --penalize_failures 0.1
-python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "case30" --export_results_file --time_limit 1.0 --budget 25 --penalize_failures 0.1
-python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "case30" --export_results_file --time_limit 1.0 --budget 50 --penalize_failures 0.1
-python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "case30" --export_results_file --time_limit 1.0 --budget 100 --penalize_failures 0.1
-python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "case30" --export_results_file --time_limit 1.0 --budget 150 --penalize_failures 0.1
-python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "case30" --export_results_file --time_limit 1.0 --budget 200 --penalize_failures 0.1
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "instance30" --export_results_file --time_limit 1.0 --budget 0
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "instance30" --export_results_file --time_limit 1.0 --budget 25
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "instance30" --export_results_file --time_limit 1.0 --budget 50
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "instance30" --export_results_file --time_limit 1.0 --budget 100
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "instance30" --export_results_file --time_limit 1.0 --budget 150
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "instance30" --export_results_file --time_limit 1.0 --budget 200
+
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "instance39" --export_results_file --time_limit 1.0 --budget 0
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "instance39" --export_results_file --time_limit 1.0 --budget 25
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "instance39" --export_results_file --time_limit 1.0 --budget 50
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "instance39" --export_results_file --time_limit 1.0 --budget 100
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "instance39" --export_results_file --time_limit 1.0 --budget 150
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "instance39" --export_results_file --time_limit 1.0 --budget 200
+
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "instance57" --export_results_file --time_limit 1.0 --budget 0
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "instance57" --export_results_file --time_limit 1.0 --budget 25
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "instance57" --export_results_file --time_limit 1.0 --budget 50
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "instance57" --export_results_file --time_limit 1.0 --budget 100
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "instance57" --export_results_file --time_limit 1.0 --budget 150
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "instance57" --export_results_file --time_limit 1.0 --budget 200
+
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "instance118" --export_results_file --time_limit 1.0 --budget 0
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "instance118" --export_results_file --time_limit 1.0 --budget 25
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "instance118" --export_results_file --time_limit 1.0 --budget 50
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "instance118" --export_results_file --time_limit 1.0 --budget 100
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "instance118" --export_results_file --time_limit 1.0 --budget 150
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "instance118" --export_results_file --time_limit 1.0 --budget 200
+
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "instance300" --export_results_file --time_limit 1.0 --budget 0
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "instance300" --export_results_file --time_limit 1.0 --budget 25
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "instance300" --export_results_file --time_limit 1.0 --budget 50
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "instance300" --export_results_file --time_limit 1.0 --budget 100
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "instance300" --export_results_file --time_limit 1.0 --budget 150
+python "c:\Users\Adi Sarid\Documents\GitHub\grid_robust_opt\main_program_one_depth_cascade.py" --instance_location "instance300" --export_results_file --time_limit 1.0 --budget 200
