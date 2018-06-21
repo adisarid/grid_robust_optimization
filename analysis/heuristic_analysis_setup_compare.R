6a8bcc7fe4824a052c50421a8e919530d38cc486
adisarid <adisarid@gmail.com>
2018-06-21 12:07:35 +0300

    Include points indicating where the algorithm jumped to a different neighborhood

--- analysis/heuristic_analysis_setup_compare.R
+++ analysis/heuristic_analysis_setup_compare.R
@@ -6,21 +6,36 @@ heuristic.compare.starttime <- heuristic %>%
   group_by(args.instance_location, budget, upgrade_selection_bias, min_neighborhoods, min_neighbors_per_neighborhood) %>%
   summarize(starttime = min(current_time))
 
+
+neighborhood.jumps <- heuristic %>%
+  mutate(rundescription = paste0("(", upgrade_selection_bias*100, "%, ", min_neighborhoods,
+                                 ", ", min_neighbors_per_neighborhood, ")")) %>%
+  group_by(args.instance_location, budget, upgrade_selection_bias, 
+           min_neighborhoods, min_neighbors_per_neighborhood,
+           neighborhoods_searched, rundescription) %>%
+  left_join(heuristic.compare.starttime) %>%
+  mutate(runtime = (current_time - starttime)/60) %>%
+  select(-starttime) %>%
+  summarize(neighborhood.jump.time = min(runtime),
+            current_time = min(current_time),
+            current_supply = min(current_supply))
+
+
 heuristic.compare <- heuristic %>%
-  mutate(rundescription = paste0("sl: ", upgrade_selection_bias, "min_nbrhds: ", min_neighborhoods,
-                                "min_nbrs_per: ", min_neighbors_per_neighborhood)) %>%
+  mutate(rundescription = paste0("(", upgrade_selection_bias*100, "%, ", min_neighborhoods,
+                                 ", ", min_neighbors_per_neighborhood, ")")) %>%
   left_join(heuristic.compare.starttime) %>%
   mutate(runtime = (current_time - starttime)/60) %>%
   select(-starttime)
 
-neighborhood.jumps <- heuristic %>%
-  group_by(args.instance_location, budget, upgrade_selection_bias, min_neighborhoods, min_neighbors_per_neighborhood) %>%
-  
-             
 
 hr.comp.plot <- ggplot(heuristic.compare, aes(y = current_supply, x = runtime)) + 
-  geom_line(aes(color = rundescription), size = 2, alpha = 0.5)
-plotly::ggplotly(hr.comp.plot)  
+  geom_line(aes(color = rundescription), size = 2, alpha = 0.5) + 
+  guides(color = guide_legend("(Selection bias,\nmin neighborhoods,\nmin neighbors per neighborhood)")) +
+  geom_point(data = neighborhood.jumps, aes(x = neighborhood.jump.time,
+                                            y = current_supply,
+                                            color = rundescription), size = 2) 
+  
 
-hr.comp.plot2 <- ggplot(heuristic.compare, aes(y = current_supply, x = neighborhoods_searched)) + 
-  geom_line(aes(color = rundescription), size = 2, alpha = 0.5)
+
+plotly::ggplotly(hr.comp.plot)  
