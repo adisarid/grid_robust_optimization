044e4a9dc5cfce03edc6808c21ce77632fed01e5
adisarid <adisarid@gmail.com>
2018-07-19 14:05:03 +0300

    Deleted old batch files

--- temprunbatch.bat
+++
@@ -1,9 +0,0 @@
-python "robustness_heuristic.py" --instance_location "instance24" --time_limit 1.0 --budget 50 --upgrade_selection_bias 0.25
-python "robustness_heuristic.py" --instance_location "instance24" --time_limit 1.0 --budget 50 --upgrade_selection_bias 0.5
-python "robustness_heuristic.py" --instance_location "instance24" --time_limit 1.0 --budget 50 --upgrade_selection_bias 0.75
-
-python "robustness_heuristic.py" --instance_location "instance24" --time_limit 1.0 --budget 50 --min_neighbors 10
-python "robustness_heuristic.py" --instance_location "instance24" --time_limit 1.0 --budget 50 --min_neighbors 25
-python "robustness_heuristic.py" --instance_location "instance24" --time_limit 1.0 --budget 50 --min_neighbors 50
-
-python "robustness_heuristic.py" --instance_location "instance24" --time_limit 1.0 --budget 50 --min_neighborhoods_total 50
