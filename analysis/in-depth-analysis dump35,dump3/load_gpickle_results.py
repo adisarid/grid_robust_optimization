a7cdbcf4e4a0ac28468b4afc8790c2e0a1919aad
adisarid <adisarid@gmail.com>
2018-08-20 23:50:39 +0300

    Improved translation of gpickle to csv with for loop

--- analysis/in-depth-analysis dump35,dump3/load_gpickle_results.py
+++ analysis/in-depth-analysis dump35,dump3/load_gpickle_results.py
@@ -1,12 +1,11 @@
 import networkx as nx
+import csv
 
 # load the gpickle file
-G = nx.read_gpickle("3.0.gpickle")
-
-all_edges_with_info = [[cur_edge[0], cur_edge[1], G.edges[cur_edge]['capacity']] for cur_edge in G.edges]
-
-import csv
-with open("grid_edges_heuristic_solution.csv", "wb") as fp:
-    wr = csv.writer(fp, dialect='excel')
-    wr.writerow(['edge_i','edge_j', 'capacity'])
-    wr.writerows(all_edges_with_info)
+for grid_num in range(1, 17):
+    G = nx.read_gpickle('C:/Users/Adi Sarid/Documents/GitHub/grid_robust_opt/analysis/Nominal results/detailed_results/' + str(grid_num) + '.0.gpickle')
+    all_edges_with_info = [[cur_edge[0], cur_edge[1], G.edges[cur_edge]['capacity']] for cur_edge in G.edges]
+    with open("C:/Users/Adi Sarid/Documents/GitHub/grid_robust_opt/analysis/Nominal results/detailed_results/grid_edges_heuristic_solution" + str(grid_num) + ".csv", "wb") as fp:
+        wr = csv.writer(fp, dialect='excel')
+        wr.writerow(['edge_i','edge_j', 'capacity'])
+        wr.writerows(all_edges_with_info)
