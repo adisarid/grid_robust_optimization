fa538e39a8a3af401917c7cd937238030774d650
adisarid <adisarid@gmail.com>
2018-08-20 23:50:12 +0300

    using str_detect instead of join to match algorithm type

--- analysis/compare_algorithms.R
+++ analysis/compare_algorithms.R
@@ -9,12 +9,9 @@ source("analysis/grid_sunken_cost.R")
 res <- read_csv("Nominal results/dump.csv", col_names = c("dump", "value", "runtime")) %>%
   full_join(readxl::read_excel("new.batch.parameters.xlsx"), by = c("dump" = "dump_file")) %>%
   mutate(percent_supplied = value/tot.demand) %>%
-  left_join(tibble(algorithm = c("robustness_heuristic_upper_bound.py",
-                                 "main_program.py --mip_emphasis 1",
-                                 "main_program_one_depth_cascade.py"),
-                   algorithm.name = c("LNS",
-                                      "Lazy",
-                                      "One.depth"))) %>%
+  mutate(algorithm.name = case_when(str_detect(runcommand, "robustness_heuristic_upper_bound.py") ~ "LNS",
+                                       str_detect(runcommand, "main_program.py") ~ "Lazy",
+                                       str_detect(runcommand, "one_depth") ~ "One.depth")) %>%
   left_join(potential.edges) %>%
   mutate(line_establish_cost = line_establish_cost_coef_scale + upgrade.cost*line_establish_capacity_coef_scale,
          line_upgrade_cost = upgrade.cost*line_upgrade_capacity_coef_scale) %>%
