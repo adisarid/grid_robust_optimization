a8db49cbda7254126fce1233c85d006623cb0856
adisarid <adisarid@gmail.com>
2018-08-21 23:25:28 +0300

    Added computation of brute force upper bound

--- compute_grid_supply_from_gpickle.py
+++ compute_grid_supply_from_gpickle.py
@@ -1,7 +1,8 @@
 # A very simple script used to load a gpickle power grid,
 # load the failure scenarios, and then write the supply per scenario output.
 
-from robustness_heuristic_upper_bound import read_scenarios, compute_current_supply
+from robustness_heuristic_upper_bound import read_scenarios, compute_current_supply, read_nodes, read_edges
+from robustness_heuristic_upper_bound import create_power_grid
 
 # ************************************************
 # ********* Import relevant libraries ************
@@ -12,6 +13,7 @@ import argparse
 import os
 import csv
 import networkx as nx
+import warnings
 
 parser = argparse.ArgumentParser(description="Load a gpickle power grid and compute supply")
 parser.add_argument('--instance_location', help="Provide the location for instance files (directory name)",
@@ -20,6 +22,15 @@ parser.add_argument('--gpickle_location', help="The location of a gpickle file",
                     type = str, default="")
 parser.add_argument('--output_file', help="Where should I save a csv with the results?",
                     type = str, default = "c:/temp/grid_cascade_output/tmp_supply_gpickle.csv")
+parser.add_argument('--brute_force_upper_bound', help = "Compute a brute force upper bound by exhausting all"
+                                                        "installments. Ignores the gpickle_location.",
+                    action = "store_true")
+parser.add_argument('--line_upgrade_capacity_coef_scale',
+                    help = "Capacity coefficient for new transmission lines and upgrades",
+                    type = float, default = 5.0)
+parser.add_argument('--line_establish_capacity_coef_scale',
+                    help = "Coefficient for scaling capacity of newely established transmission lines",
+                    type = float, default = 5.0)
 
 # ... add additional arguments as required here ..
 args = parser.parse_args()
@@ -30,8 +41,34 @@ instance_location = os.getcwd() + '\\' + args.instance_location + '\\'
 scenarios = read_scenarios(instance_location + 'scenario_failures.csv',
                            instance_location + 'scenario_probabilities.csv')
 
-# load the gpickle file
-G = nx.read_gpickle(os.getcwd() + '\\' + args.qpickle_location)
+if (args.gpickle_location <> "") and args.brute_force_upper_bound:
+    warnings.warn("I have received both gpickle location and a brute force computation request."
+                  "Ignoring gpickle_location.")
+
+if not args.brute_force_upper_bound:
+    # load the gpickle file
+    G = nx.read_gpickle(os.getcwd() + '\\' + args.qpickle_location)
+else:
+    # load the grid from original files and install everything
+    nodes = read_nodes(instance_location + 'grid_nodes.csv')
+    original_edges = read_edges(instance_location + 'grid_edges.csv')
+    G = create_power_grid(nodes, original_edges)
+    # edges for establishment
+    establishable_edges = [(edge[1], edge[2]) for edge in original_edges if edge[0] == 'H' and original_edges[edge] > 0
+                           and not G.has_edge(edge[1], edge[2])]
+    # list all edges which are upgradable
+    upgradable_edges = [(edge[1], edge[2]) for edge in original_edges if edge[0] == 'H' and original_edges[edge] == 0]
+
+    add_edges_1 = [(cur_edge[0], cur_edge[1], {
+        'capacity': args.line_upgrade_capacity_coef_scale + args.line_establish_capacity_coef_scale,
+        'susceptance': original_edges[('x',) + cur_edge],
+        'establish_cost': original_edges[('H',) + cur_edge],
+        'upgrade_cost': original_edges[('h',) + cur_edge]})
+                   for cur_edge in establishable_edges]
+    G.add_edges_from(add_edges_1)
+
+    for edge_to_upgrade in upgradable_edges:
+        G.edges[edge_to_upgrade]['capacity'] += args.line_upgrade_capacity_coef_scale
 
 current_grid_outcome = compute_current_supply(G)
 
